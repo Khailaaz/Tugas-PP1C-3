69d95b754661f109f042a862575b4bb907a2f62b
Khailaaz <khailaazharaputri@gmail.com>
2024-10-05 19:58:56 +0700

    khaila 3_4.cpp

--- khaila 3_4.cpp
+++ khaila 3_4.cpp
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-int main()
-{
-	int angka;
-		printf("Masukkan bilangan bulat positif: ");
-		scanf("%d", angka);
-		
-		if(angka <= 0) {
-			printf("mohon masukkan bilangan bulat positif.\n");
-		}
-		else{
-			if (angka % 2 == 0){
-				printf("GENAP\n");
-			}
-			else{
-				printf("GANJIL\n");
-			}
-		}
-		return 0;
+int main() {
+    int bilangan;
+
+    printf("Masukkan bilangan bulat positif: ");
+    scanf("%d", &bilangan);
+
+    if (bilangan <= 0) {
+        printf("Silakan masukkan bilangan bulat positif.\n");
+        scanf("%d", &bilangan);
+    } else {
+        if (bilangan % 2 == 0) {
+            printf("GENAP\n");
+        } else {
+            printf("GANJIL\n");
+        }
+    }
+
+    return 0;
 }
